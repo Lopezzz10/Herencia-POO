fb2867791260339e522d00ec79af9a605c4118c9
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-09-24 14:07:21 -0500

    Clase Object_Método toString

---
+++ src/com/krakedev/herencia/test/TestToString.java
@@ -0,0 +1,14 @@
+package com.krakedev.herencia.test;
+
+import com.krakedev.herencia.Padre;
+
+public class TestToString {
+
+	public static void main(String[] args) {
+		Padre p = new Padre();
+		System.out.println(p);
+		p.toString();
+		System.out.println(p.toString());
+	}
+
+}
