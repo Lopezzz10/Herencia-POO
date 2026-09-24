403eac50ca940be2a38bdc5cb3488d4fcac0b0f2
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-09-24 15:24:40 -0500

    Test_Ejecuta Constructor Vacío Heredado

--- src/com/krakedev/herencia/Hija.java
+++ src/com/krakedev/herencia/Hija.java
@@ -1,10 +1,12 @@
 package com.krakedev.herencia;
 
 public class Hija extends Padre {
+	
+	public Hija () {
+		super();
+	}
 	public void escucharBadBunny() {
 		System.out.println("Escuchando esta musica horrible");
-		
-		
 	}
 
 	@Override
